03be0b031721ae8b9b9310ba4cc31b4cdd915b60
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-26 09:41:26 -0700

    First Commit

---
+++ ColorEqualsRepro/App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ColorEqualsRepro
+{
+	public class App : Application
+	{
+		public App() => MainPage = new MainPage();
+	}
+
+	public class MainPage : ContentPage
+	{      
+		readonly  Button _toggleColorButton;
+		public MainPage()
+		{
+			_toggleColorButton = new Button { Text = "Toggle Background Color" };
+			_toggleColorButton.Clicked += HandleColorButtonClicked;
+
+			Content = _toggleColorButton;
+		}      
+		void HandleColorButtonClicked(object sender, EventArgs e)
+		{
+			if (BackgroundColor.Equals(ColorConstants.LightBlue))
+			{
+				BackgroundColor = ColorConstants.DarkBlue;
+				_toggleColorButton.TextColor = Color.White;
+				_toggleColorButton.Text = $"Change Color to {nameof(ColorConstants.LightBlue)}";
+			}
+			else
+			{
+				BackgroundColor = ColorConstants.LightBlue;
+				_toggleColorButton.TextColor = Color.Black;
+				_toggleColorButton.Text = $"Change Color to {nameof(ColorConstants.DarkBlue)}";
+			}
+		}
+	}
+}
---
+++ ColorEqualsRepro/ColorConstants.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace ColorEqualsRepro
+{
+	public static class ColorConstants
+	{
+		#region Fields
+		static Color _darkBlue, _lightBlue;
+		#endregion
+        
+		#region Properties
+		public static Color DarkBlue => _darkBlue == default ? (_darkBlue = Color.FromHex("2980b9")) : _darkBlue;
+		public static Color LightBlue => _lightBlue.Equals(default) ? (_lightBlue = Color.FromHex("3498db")) : _lightBlue;
+		#endregion
+	}
+}
