c619e926c56d09224cfe823968f1bebd9a566e89
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-26 10:04:26 -0700

    Updated Sample for Android

--- ColorEqualsRepro/App.cs
+++ ColorEqualsRepro/App.cs
@@ -10,28 +10,35 @@ namespace ColorEqualsRepro
 	}
 
 	public class MainPage : ContentPage
-	{      
-		readonly  Button _toggleColorButton;
+	{
+		readonly Button _toggleColorButton;
 		public MainPage()
 		{
-			_toggleColorButton = new Button { Text = "Toggle Background Color" };
+			_toggleColorButton = new Button
+			{
+				Text = "Toggle Background Color",
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center
+			};
 			_toggleColorButton.Clicked += HandleColorButtonClicked;
 
 			Content = _toggleColorButton;
-		}      
+		}
 		void HandleColorButtonClicked(object sender, EventArgs e)
 		{
 			if (BackgroundColor.Equals(ColorConstants.LightBlue))
 			{
-				BackgroundColor = ColorConstants.DarkBlue;
-				_toggleColorButton.TextColor = Color.White;
-				_toggleColorButton.Text = $"Change Color to {nameof(ColorConstants.LightBlue)}";
+				if (Device.RuntimePlatform.Equals(Device.iOS))
+					_toggleColorButton.TextColor = Color.White;
+				
+				_toggleColorButton.Text = $"Change Background Color to {nameof(ColorConstants.LightBlue)}";
+                BackgroundColor = ColorConstants.DarkBlue;
 			}
 			else
 			{
+                _toggleColorButton.TextColor = Color.Black;
+                _toggleColorButton.Text = $"Change Background Color to {nameof(ColorConstants.DarkBlue)}";
 				BackgroundColor = ColorConstants.LightBlue;
-				_toggleColorButton.TextColor = Color.Black;
-				_toggleColorButton.Text = $"Change Color to {nameof(ColorConstants.DarkBlue)}";
 			}
 		}
 	}
